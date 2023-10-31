426e6efd8e8f5ed50780fb2b7237f7e61fd4a744
Abel Naharro <abelnhm92@gmail.com>
2023-10-31 23:41:45 +0100

    Fix Vercel file

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,7 @@
 import { Header } from '../header/header';
 import { FormPersonalData } from '../form-personal/form.personal';
 import './app.scss';
-import { Footer } from '../footer/footer';
+import { Footer } from '../footer/Footer';
 const title = 'My page';
 const text = 'Isdi Coders';
 
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import './footer.scss';
 type Props = {
   text: string;
 };
-
+//
 export function Footer({ text }: Props) {
   return (
     <footer>
